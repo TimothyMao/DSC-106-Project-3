5f95c3b5566e0aae7c8e7e443c92cd16670d4cad
Takafumi <tamatsui@ucsd.edu>
2025-02-15 18:50:47 -0800

    Updated the EDA section and added files for the website

---
+++ main.js
@@ -0,0 +1,135 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadData();
+    createLineplot();
+  });
+  
+async function loadData() {
+    const data = await d3.csv('mouse.csv', (row) => ({
+        time: +row.time,
+        day: +row.day,
+        hour: +row.hour,
+        is_night: row.is_night === '1',
+        is_estrus: row.is_estrus === '1',
+        m1_act: +row.m1_act,
+        m2_act: +row.m2_act,
+        m3_act: +row.m3_act,
+        m4_act: +row.m4_act,
+        m5_act: +row.m5_act,
+        m6_act: +row.m6_act,
+        m7_act: +row.m7_act,
+        m8_act: +row.m8_act,
+        m9_act: +row.m9_act,
+        m10_act: +row.m10_act,
+        m11_act: +row.m11_act,
+        m12_act: +row.m12_act,
+        m13_act: +row.m13_act,
+        f1_act: +row.f1_act,
+        f2_act: +row.f2_act,
+        f3_act: +row.f3_act,
+        f4_act: +row.f4_act,
+        f5_act: +row.f5_act,
+        f6_act: +row.f6_act,
+        f7_act: +row.f7_act,
+        f8_act: +row.f8_act,
+        f9_act: +row.f9_act,
+        f10_act: +row.f10_act,
+        f11_act: +row.f11_act,
+        f12_act: +row.f12_act,
+        f13_act: +row.f13_act,
+        m1_temp: +row.m1_temp,
+        m2_temp: +row.m2_temp,
+        m3_temp: +row.m3_temp,
+        m4_temp: +row.m4_temp,
+        m5_temp: +row.m5_temp,
+        m6_temp: +row.m6_temp,
+        m7_temp: +row.m7_temp,
+        m8_temp: +row.m8_temp,
+        m9_temp: +row.m9_temp,
+        m10_temp: +row.m10_temp,
+        m11_temp: +row.m11_temp,
+        m12_temp: +row.m12_temp,
+        m13_temp: +row.m13_temp,
+        f1_temp: +row.f1_temp,
+        f2_temp: +row.f2_temp,
+        f3_temp: +row.f3_temp,
+        f4_temp: +row.f4_temp,
+        f5_temp: +row.f5_temp,
+        f6_temp: +row.f6_temp,
+        f7_temp: +row.f7_temp,
+        f8_temp: +row.f8_temp,
+        f9_temp: +row.f9_temp,
+        f10_temp: +row.f10_temp,
+        f11_temp: +row.f11_temp,
+        f12_temp: +row.f12_temp,
+        f13_temp: +row.f13_temp
+    }));
+
+    displayStats();
+}
+
+async function createLineplot() {
+    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const data = await d3.csv('mouse.csv', d3.autoType);
+
+    const x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.time))
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d3.max(Object.values(d).slice(3, 16)))])
+        .range([height, 0]);
+
+    const line = d3.line()
+        .x(d => x(d.time))
+        .y(d => y(d.value));
+
+    function updatePlot(group, individual, period, time, id, dataType) {
+        const filteredData = data.filter(d => {
+            if (group === 'Individual' && individual === 'male') {
+                return d[`m${id}_${dataType}`] !== undefined;
+            } else if (group === 'Individual' && individual === 'female') {
+                return d[`f${id}_${dataType}`] !== undefined;
+            } else if (group === 'Sex' && individual === 'male') {
+                return d[`m${id}_${dataType}`] !== undefined;
+            } else if (group === 'Sex' && individual === 'female') {
+                return d[`f${id}_${dataType}`] !== undefined;
+            }
+            return false;
+        });
+
+        const plotData = filteredData.map(d => ({
+            time: d.time,
+            value: d[`m${id}_${dataType}`] || d[`f${id}_${dataType}`]
+        }));
+
+        svg.selectAll("*").remove();
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x));
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        svg.append("path")
+            .datum(plotData)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+    }
+
+    // Example usage
+    updatePlot('Individual', 'male', 'All', 'All', 1, 'act');
+}
